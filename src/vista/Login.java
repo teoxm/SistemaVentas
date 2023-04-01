aed77f82572fbe81a5a82467b989cf08028cd07c
David Mendez <davidm10339@gmail.com>
2023-04-01 15:26:08 -0500

    iniciacion de nuestro proyecto

--- src/vista/Login.java
+++ src/vista/Login.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vista;
 
-/**
- *
- * @author User
- */
-public class Login {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Login extends JFrame{
+
+    public Login() {
+        
+    }
     
+    private void initComponents(){
+        jPanel2 = new JPanel();
+        jLabel2= new JLabel();
+        jLabel3 = new JLabel();
+        jLabel4 = new JLabel();
+        txtCorreo = new JTextField();
+        txtPass = new JPasswordField();
+        btnIniciar = new JButton();
+        jPanel3 = new JPanel();
+        jLabel5 = new JLabel();
+        jPanel4 = new JPanel();
+        jLabel6 = new JLabel();
+        jPanel1 = new JPanel();
+        jLabel7 = new JLabel();
+        jLabel8 = new JLabel();
+        jLabel9 = new JLabel();
+        jButton1 = new JButton();
+        jPanel5  = new JPanel();
+        jLabel1 = new JLabel();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        
+       
+    }
+    
+    //Variables declaration
+    private JButton btnIniciar;
+    private JButton jButton1;
+    private JLabel jLabel1;
+    private JLabel jLabel2;
+    private JLabel jLabel3;
+    private JLabel jLabel4;
+    private JLabel jLabel5;
+    private JLabel jLabel6;
+    private JLabel jLabel7;
+    private JLabel jLabel8;
+    private JLabel jLabel9;
+    private JPanel jPanel1;
+    private JPanel jPanel2;
+    private JPanel jPanel3;
+    private JPanel jPanel4;
+    private JPanel jPanel5;
+    private JTextField txtCorreo;
+    private JPasswordField txtPass;
 }
